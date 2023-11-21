9e1c7563a78d6cad1ab5bc2520a898a5ab9f0e02
Mantelis <mantasjasikenas@gmail.com>
2023-11-21 17:23:52 +0200

    Remove unnecessary operation from launch script.

--- build/launch.sh
+++ build/launch.sh
@@ -1,4 +1,3 @@
 echo "Launching drawing competion container"
 docker logout
-git clone https://github.com/mantasjasikenas/drawing-competition.git
 docker-compose up --build --force-recreate --no-deps -d
